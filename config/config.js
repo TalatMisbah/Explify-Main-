fcfbbd6fa234f46de539a8a17618a7231ccd5636
TalatMisbah <talat.misbah_cs18@gla.ac.in>
2021-08-22 21:19:34 +0530

    first commit

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://talat:explify@cluster0.mz6jq.mongodb.net/explify?retryWrites=true&w=majority",
+    MONGOURI:"mongodb+srv://explify:explify@cluster0.mz6jq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     JWT_SECRET:"HELLO SECRET"
 }
